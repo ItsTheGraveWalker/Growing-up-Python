b57fbe34f9189628012005679c31777a3f6296d1
ItsTheGraveWalker <60240733+ItsTheGraveWalker@users.noreply.github.com>
2020-03-23 08:14:17 -0400

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,1 +1,2 @@
 # This file was created through the command line. 
+print("This program is designed to count the amount the individual words in your document and display them on a chart for analysis.")
