044503aa7da211c0be1431c8122c70e618e91fb6
JW <darthsidious@Mikes-MacBook-Pro.local>
2020-03-23 08:18:37 -0400

    update to benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,2 +1,3 @@
 # This file was created through the command line. 
 print("This program is designed to count the amount the individual words in your document and display them on a chart for analysis.")
+# We were unable to run this program due to 'invalid syntax' error
